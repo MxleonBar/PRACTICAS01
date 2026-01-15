feb2233a1cd9d9fc08e2c1ee1e5fa72e3a3e09ff
Mario Leon <mxleonbarraza@gmail.com>
2026-01-15 16:50:33 -0700

    se termino practica 01

---
+++ js/tabla.js
@@ -0,0 +1,48 @@
+//Declaracion de Objetos
+const txtId = document.getElementById('txtId');
+const txtNombre = document.getElementById('txtNombre');
+const txtEscuela = document.getElementById('txtEscuela');
+
+const btnAgregar = document.getElementById('btnAgregar');
+const tabla = document.getElementById('tabla');
+const tbody = document.getElementById('tbody');
+
+// declaracion de funciones
+function agregar(){
+    //validar
+    let id = txtId.value;
+    let nombre = txtNombre.value;
+    let escuela = txtEscuela.value;
+
+    if(!id || !nombre || !escuela){
+        alert("faltaron datos por capturar");
+        return;
+    }
+    
+    //agregar a la tabla
+
+    const fila = document.createElement('tr');
+    
+    const c1 = document.createElement('td');
+        c1.textContent = id;
+        fila.appendChild(c1);
+
+    const c2 = document.createElement('td');
+        c2.textContent = nombre;
+        fila.appendChild(c2);
+
+    const c3 = document.createElement('td');
+        c3.textContent = escuela;
+        fila.appendChild(c3);
+
+      tbody.appendChild(fila);
+      tabla.appendChild(tbody);
+
+    //Limpiar los campos
+    txtId.value = "";
+    txtEscuela.value = "";
+    txtNombre.value = "";
+
+}
+
+btnAgregar.addEventListener('click', agregar);
